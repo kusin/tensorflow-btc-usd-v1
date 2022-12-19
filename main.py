9d4b488be0331d8b6cd6c86e11ef173641bbb9b0
kusin <aryajayaalamsyah@gmail.com>
2022-12-19 22:39:41 +0700

    -

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ import scipy.stats as sc
 import statsmodels.api as sm
 from statsmodels.graphics.tsaplots import plot_pacf
 from statsmodels.graphics.tsaplots import plot_acf
-        
+
 # pustaka untuk membuat data latih dan data uji.
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
@@ -56,7 +56,69 @@ from math import sqrt
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 
+# pustaka dashboard
+import streamlit as st
+import plotly.express as px
+
+## ---------------------------------------------------------- ##
+## - CONFIG ------------------------------------------------- ##
+## ---------------------------------------------------------- ##
+# set config streamlit
+st.set_page_config(page_title="My Dashboard", layout="wide", initial_sidebar_state="auto");
+
 ## ---------------------------------------------------------- ##
 ## - AKUISISI DATA ------------------------------------------ ##
 ## ---------------------------------------------------------- ##
+# fungsi load dataset
+@st.cache
+def load_dataset():
+    # return value
+    return pd.read_csv("D:/latihan-time-series/dataset/BTC-USD.csv");
 
+# membaca dataset csv
+dataset = load_dataset();
+
+# set index tanggal
+# dataset = dataset.set_index("Date");
+
+
+## ---------------------------------------------------------- ##
+## - Sidebar ------------------------------------------------- ##
+## ---------------------------------------------------------- ##
+# st.sidebar.title("My Dashboard");
+with st.sidebar:
+    st.title("My Dashboard");
+    st.selectbox('Choose Dataset', ('Bitcoin USD (BTC-USD)', 'Microsoft Corporation (MSFT)', 'Apple Inc. (AAPL)', 'Google Inc. (GOOG)', 'Amazon.com Inc. (AMZN)'));
+    st.date_input("Start date");
+    st.date_input("End date");
+    st.selectbox('Choose Algorithm', ('LSTM-RNN', 'GRU-RNN', 'Xgboost', 'Prophet'));
+    
+    st.button('Submit');
+    
+
+## ---------------------------------------------------------- ##
+## - Header ------------------------------------------------- ##
+## ---------------------------------------------------------- ##
+# set a title dashboard
+st.markdown('## Stock Price Prediction with LSTM - GRU - Xgboost - Prophet');
+
+
+## ---------------------------------------------------------- ##
+## - Content ------------------------------------------------ ##
+## ---------------------------------------------------------- ##
+# show a dataset
+st.text("Dataset of Bitcoin USD (BTC-USD)");
+st.dataframe(dataset, use_container_width=True);
+
+# plot time series a colums open and close
+st.plotly_chart(px.line(dataset, x='Date', y='Open', title='Open price bitcoin'),
+                use_container_width=True, sharing="streamlit", theme="streamlit");
+
+st.plotly_chart(px.line(dataset, x='Date', y='Close', title='Close price bitcoin'),
+                use_container_width=True, sharing="streamlit", theme="streamlit");
+
+st.plotly_chart(px.line(dataset, x='Date', y='High', title='High price bitcoin'),
+                use_container_width=True, sharing="streamlit", theme="streamlit");
+
+st.plotly_chart(px.line(dataset, x='Date', y='Low', title='Low price bitcoin'),
+                use_container_width=True, sharing="streamlit", theme="streamlit");
